54090c6e7edee768ab0c6bb1524b267468d8cbf9
panditrahulsharma <rahulpandit151197@gmail.com>
2020-02-22 12:26:38 +0530

    Register and login form using Flask

--- login_register/app.py
+++ login_register/app.py
@@ -1,5 +1,8 @@
 from flask import Flask,render_template,request,redirect,url_for
 import MySQLdb
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 app = Flask(__name__)
 
@@ -11,15 +14,29 @@ def index():
 
 @app.route("/signUp", methods=['GET', 'POST'])
 def signUp():
-	username = str(request.form["uname"])
-	password = str(request.form["pswd"])
-	email = str(request.form["email"])
-	
-	cursor = conn.cursor()
-	
-	cursor.execute("INSERT INTO user (name,password,email)VALUES(%s,%s,%s)",(username,password,email))
-	conn.commit()
-	return redirect(url_for("login"))
+	try:
+		if request.method=='POST':
+			username = str(request.form["uname"])
+			password = str(request.form["pswd"])
+			email = str(request.form["email"])
+
+			cursor = conn.cursor() #make a pointer
+			cursor.execute("SELECT name FROM user WHERE email ='"+email+"'") #check user is exist or not
+			user = cursor.fetchall()
+			
+			if len(user) >0: #if length is greater 1 then user email is exist
+				return render_template("signup.html", title="User already Exist please try again another email")
+			
+			else:      #user not exist insert detail in database
+				cursor.execute("INSERT INTO user (name,password,email)VALUES(%s,%s,%s)",(username,password,email))
+				conn.commit()
+				return redirect(url_for("login"))		
+			
+		else:
+			pass
+	except Exception as e:
+		return render_template("signup.html", title=e)
+
 	
 
 @app.route("/login")
@@ -31,18 +48,28 @@ def login():
 
 @app.route("/checkUser",methods=["POST"])
 def check():
-	email = str(request.form["email"])
-	password = str(request.form["pswd"])
-	cursor = conn.cursor()
-	cursor.execute("SELECT name FROM user WHERE email ='"+email+"'")
-	user = cursor.fetchall()
-	
-	if len(user) >0:
-		for value in user:
-		    print(value)		
-		return redirect(url_for("home"))
-	else:
-		return "failed"
+	try:
+		if request.method=='POST':
+			email = str(request.form["email"])
+			password = str(request.form["pswd"])
+
+			cursor = conn.cursor()
+			cursor.execute("SELECT name FROM user WHERE email ='"+email+"' and password='"+password+"' ")
+			user = cursor.fetchall()
+			
+			if len(user) >0:
+				for value in user:
+				    print(value)		
+				return redirect(url_for("home"))
+			else:
+				return render_template("signup.html", title="Invalid details")	
+			
+		else:
+			pass
+	except Exception as e:
+		return render_template("signup.html", title="Try again")
+
+
 
 
 
