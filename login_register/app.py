786f135a2dd65c720f663fbd16e064b4b37c084d
panditrahulsharma <rahulpandit151197@gmail.com>
2020-02-22 16:32:51 +0530

    Create session in Flask

--- login_register/app.py
+++ login_register/app.py
@@ -1,11 +1,21 @@
+#rahul pandit
+
 from flask import *
 from flask import Flask, request, jsonify, json, make_response, redirect, session, send_from_directory
 import MySQLdb
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from datetime import timedelta #for time to define how long session is active
 
+#create session in flask 
+#https://www.youtube.com/watch?v=iIhAfX4iek0
+#https://techwithtim.net/tutorials/flask/sessions/
 
 app = Flask(__name__)
+
+app.permanent_session_lifetime = timedelta(days=5)
+
+app.secret_key = "hello" #encrypt data by this key on server
 
 conn = MySQLdb.connect(host="localhost",user="root",password="Root@123",db="rahul")
 
@@ -29,6 +39,7 @@ def signUp():
 				return render_template("signup.html", title="User already Exist please try again another email")
 			
 			else:      #user not exist insert detail in database
+
 				cursor.execute("INSERT INTO user (name,password,email)VALUES(%s,%s,%s)",(username,password,email))
 				conn.commit()
 				return redirect(url_for("login"))		
@@ -60,25 +71,50 @@ def check():
 			
 			if len(user) >0:
 				for value in user:
-				    print(value)	
-				    #https://stackoverflow.com/questions/26954122/how-can-i-pass-arguments-into-redirecturl-for-of-flask
+					print(value)	
+					#https://stackoverflow.com/questions/26954122/how-can-i-pass-arguments-into-redirecturl-for-of-flask
+				session.permanent = True 
+				session['email']=email #if i user is exist at login time then create session and redirect to profile
 
-				return redirect(url_for("home",data=user)) #data is a variable you can use any to send for home page
+				return redirect(url_for("user")) #data is a variable you can use any to send for home page
+
 			else:
-				return render_template("signup.html", title="Invalid details")	
+				return render_template("login.html", title="Invalid details")	
 			
 		else:
-			pass
+			if "email" in session:  #when user try to go login page it automatically redirect to profile page
+				return redirect(url_for("user"))
+
+			return render_template("login.html")
+
 	except Exception as e:
-		return render_template("signup.html", title="Try again")
+		return render_template("login.html", title="Try again") #any excepton and error occured redirect page into register
 
 
 
 
 
-@app.route("/home")
-def home():
-	return render_template("home.html",user_data=request.args.get('data')) #don't mind user_data its random name you can use your own this is reflect on home page
+
+@app.route("/user")
+def user():
+	if "email" in session:
+		email = session["email"]
+
+		#now fetch all information of users
+		cursor = conn.cursor()
+		cursor.execute("SELECT * FROM user WHERE email ='"+email+"' ")
+		account = cursor.fetchall()
+		print(account)
+		return render_template("home.html",user_info=account[0])
+	else:
+		return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
 	app.run(debug=True,port=4000)
