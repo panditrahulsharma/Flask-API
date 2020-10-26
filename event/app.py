576ddb2735c0c2b2d972dd05b5caee66ad2dbdf4
panditrahulsharma <rahulpandit151197@gmail.com>
2020-10-26 10:16:59 +0530

    event of on change in javascript

--- event/app.py
+++ event/app.py
@@ -1,37 +1,46 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from flask import Flask, request, jsonify, json, make_response, redirect, session, send_from_directory,flash
 
-import random 
+import pandas as pd
+import json
 
-from time import sleep
+from pyutil import filereplace
+
+
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return render_template('dynamic.html')
-
-
-@app.route('/ajax_get_data',methods=['GET','POST'])
-def ajaxdata():
-	send_data = [{"name":"00.01.23","length":"00.03.23","leng":"dqdqwd","status":'sucess'},{"name":"00.23.23","length":"00.13.23","leng":"en-gb","upload":"8-oct","status":'sucess'}];
-	# send_data = [{"name":"00.01.23","length":"00.03.23","leng":"en-gb","upload":"7-oct","status":'sucess'}];
-
-	return make_response(jsonify(send_data), 200)
+@app.route('/thanks')
+def thanks():
+	return render_template('subtitle.html')
 
 
 @app.route('/ajax_post',methods=['GET','POST'])
 def ajaxpost():
 	print("request comming ...",request.form['new_value'])
-	send_data = {"name":request.form['new_value'],"sucess":True}
-	sleep(1)
+
+	old_data=request.form['old_value']
+	new_data=request.form['new_value']
+
+	print("old_data=",old_data)
+	print("new_data=",new_data)
+
+	fileName = './static/rpa.srt'
+	# Read in the file
+	with open(fileName) as r:
+	  text = r.read().replace(old_data,new_data)
+	with open(fileName, "w") as w:
+	  w.write(text)
+
+
+	send_data = {"data1":new_data,"data2":new_data,"sucess":True}
 	return make_response(jsonify(send_data), 200)
 
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
-    
+if __name__ == '__main__':
+	app.run(debug=True,host='localhost')
+
